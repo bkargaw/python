981d8dde06c5cc102fecbd23a886509044000649
Bruk Argaw <bkargaw@gmail.com>
2017-08-03 15:58:20 -0700

    finished working with the file

--- intro_to_python/Ch4/files_start.py
+++ intro_to_python/Ch4/files_start.py
@@ -2,7 +2,27 @@
 # Read and write files using the built-in Python file methods
 # (For Python 3.x, be sure to use the ExampleSnippets3.txt file)
 
-def main():  
-    
+def main():
+    # f = open('textfile.txt', "w+")
+    # for i in range(10):
+    #     f.write("this is line %d\r\n" % (i+1))
+    #
+    # f.close()
+    #
+    # f = open('textfile.txt', "a+")
+    # for i in range(10):
+    #     f.write("this is line %d\r\n" % (i+1))
+    #
+    # f.close()
+
+    f = open('textfile.txt', "r")
+    if f.mode == 'r':
+        # content = f.read()
+        # print content
+        
+        fl = f.readlines()
+        for line in fl:
+            print line
+    f.close()
 if __name__ == "__main__":
   main()
