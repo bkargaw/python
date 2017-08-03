bccdc79853b0119d5047fb8ac1ea5d3b7c46e744
Bruk Argaw <bkargaw@gmail.com>
2017-08-03 15:13:14 -0700

    finished working with imported modules in pyton

--- intro_to_python/Ch3/dates_start.py
+++ intro_to_python/Ch3/dates_start.py
@@ -2,10 +2,19 @@
 # Example file for working with date information
 # (For Python 3.x, be sure to use the ExampleSnippets3.txt file)
 
+from datetime import date
+from datetime import time
+from datetime import datetime
 
 def main():
-  
-  
+    # today = date.today()
+    # print   "the date is ", today
+    # print "the date components are ", today.day, today.month,today.year
+    # print "today's date number is ", today.weekday()
+
+    today = datetime.now()
+    print   "the date is ", today
+    print 'the current time is ', datetime.time(today)
+
 if __name__ == "__main__":
   main();
-  
--- intro_to_python/Ch3/formatting_start.py
+++ intro_to_python/Ch3/formatting_start.py
@@ -5,6 +5,13 @@
 from datetime import datetime
 
 def main():
+    now = datetime.now()
+    # print 'this is now', now
+    # print "the year is", now.strftime('%Y')
+    # print now.strftime('%a,%d,%B,%Y')
+    print now.strftime('%c')
+    print now.strftime('%X')
+    print now.strftime('%x')
 
 if __name__ == "__main__":
   main();
